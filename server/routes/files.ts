d8d27d3c2a47af1cb615e4498341dac2f5c17aa6
DY-2001 <dushyant.y20@iiits.in>
2024-07-15 22:01:41 +0530

    file upload logic

--- server/routes/files.ts
+++ server/routes/files.ts
@@ -1,16 +1,46 @@
 import express from 'express';
 import multer from 'multer';
+import {UploadApiResponse, v2 as cloudinary} from 'cloudinary'
+import File from '../models/File'
 
 const router = express.Router()
 
-const storage = multer.diskStorage({
-    
-})
+const storage = multer.diskStorage({})
 
 let upload = multer({ storage })
 
-router.get('/upload', (res, req) => {
+router.post('/upload', upload.single("myFile"), async (req, res) => {
+    try {
+        if(!req.file) {
+            return res.status(400).json({message: "No file uploaded"})
+        }
 
+        let uploadedFile: UploadApiResponse;
+        try {
+            uploadedFile = await cloudinary.uploader.upload(req.file.path, {
+            folder: "fileSource",
+            resource_type: "auto"
+        })
+        } catch (error) {
+            return res.status(400).json({message: "Cloudinary error"})
+        }
+
+        const {originalname} = req.file
+        const {secure_url, bytes,format} = uploadedFile
+
+        const file = await File.create({
+            filename: originalname,
+            sizeInBytes: bytes,
+            secure_url,
+            format
+        })
+        
+        res.status(200).json({id: file._id, downloadPageLink: `${process.env.API_BASE_ENDPOINT_CLIENT}/download/${file._id}`})
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: "Server error"})
+    }
 })
 
 export default router;
--- server/server.ts
+++ server/server.ts
@@ -2,15 +2,27 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db';
+import fileRoutes from './routes/files';
+import {v2 as cloudinary} from 'cloudinary'
 
+const PORT = process.env.PORT || 5500;
 const app = express();
 dotenv.config();
 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_API_CLOUD,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
 connectDB();
-
-
 app.use(cors());
-const PORT = process.env.PORT || 5500;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/files", fileRoutes)
+
+
 
 app.listen(PORT, () => {
     console.log(`Hello, Server is listening on port ${PORT}`);
